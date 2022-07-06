fa46e129b08c520622275bd59a1c9f688f90bae6
DocMeehanGit <docmeehanwow@gmail.com>
2022-07-06 16:39:19 -0400

    Deleted Test.py created Hello World

--- Test.py
+++
@@ -1,8 +0,0 @@
-print('Hello')
-
-def test():
-    print('test')
-
-if __name__ == "__main__":
-    test()
-    print('hello')
---
+++ basic_python.py
@@ -0,0 +1,5 @@
+print("Hello")
+
+name = input("Tell me your name\n")
+
+print("Hello, how are you today " + name)
